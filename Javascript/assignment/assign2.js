c86163e65c12b6ef8e48b8b1d0774261acdc5080
Maruti Hanumanth Naik <marutihanumanthnaik@gmail.com>
2026-03-26 22:15:28 +0530

    Today work  done in the intership 26/3/2026

--- Javascript/assignment/assign2.js
+++ Javascript/assignment/assign2.js
@@ -1,3 +1,25 @@
 let N=String(prompt("Enter The Name"));
 
-console.log(N.toUpperCase);
+console.log(N.toUpperCase());
+console.log(N.length);
+
+console.log(N.slice(-N.length,-N.length+1));
+console.log(N.slice(N.length-1));
+
+
+
+let str1=String(prompt("enter the String"));
+console.log(str1.slice(-str1.length-1,-str1.length+5));
+console.log(str1.slice(str1.length-3));
+
+
+let email1=String(prompt("enter the email"));
+let a=Boolean(email1.includes('@'));
+let b=Boolean(email1.includes('.'))
+if(a==true & b==true){
+    console.log("Valid");   
+}
+else{
+    console.log("Invalid");
+    
+}
---
+++ Javascript/stringsmethod.js
@@ -0,0 +1,25 @@
+let a="MarutiHanumanthNik"
+console.log(a.charAt(2));
+
+//reverse the strings
+console.log(a.split("").reverse().join(''));
+
+//replace and replaceall
+let b="good morining"
+console.log(b.replace('good','afternoon'));
+
+let c="hello hello nan preeti ille yello kaldehoytu chamachkaloo"
+console.log(c.replaceAll('hello','bye'))
+
+
+let num="8277516857"
+console.log(num.padStart('14','+91 '));
+
+let name="Maruti"
+console.log(name.padEnd('10','Naik'));
+
+//startwith endswith
+console.log(name.startsWith('Ma'));
+console.log(name.endsWith('ti'));
+
+
