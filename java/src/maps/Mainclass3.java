4de549c4a99efe61357dfc232b4c256634459538
Maruti Hanumanth Naik <marutihanumanthnaik@gmail.com>
2026-04-17 06:02:23 +0530

    Today work  done in the intership 16/4/2026

--- java/src/maps/Mainclass3.java
+++ java/src/maps/Mainclass3.java
@@ -1,5 +1,26 @@
 package maps;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class Car{
+	String c_name;
+	int c_cost;
+	Car(String c_name,int c_cost){
+		this.c_name=c_name;
+		this.c_cost=c_cost;
+	}
+	public String toString() {
+		return this.c_name+this.c_cost;
+	}
+}
 public class Mainclass3 {
+	LinkedHashMap <Integer,Car> c=new LinkedHashMap();
+	//c.put(1, new Car("Audi",20000));
+	c.put(1, new Car("Lenvov",1200));
+	m1.put(2, new Laptop(13000,"dell","Blue"));
+	
+	
+	
 
 }
