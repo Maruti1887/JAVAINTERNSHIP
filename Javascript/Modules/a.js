89907296bc8918f515402bbdf12f598604578af8
Maruti Hanumanth Naik <marutihanumanthnaik@gmail.com>
2026-04-24 23:17:02 +0530

    Today work  done in the intership 24/4/2026

---
+++ Javascript/Modules/a.js
@@ -0,0 +1,6 @@
+export let a=(x,y)=>{
+    return x-y;
+}
+export let b=(x,y)=>{
+    return x*y;
+}
---
+++ Javascript/Modules/b.js
@@ -0,0 +1,5 @@
+import {a,b} from './a.js';
+console.log(a(10,5));
+console.log(b(10,5));
+console.log();
+
